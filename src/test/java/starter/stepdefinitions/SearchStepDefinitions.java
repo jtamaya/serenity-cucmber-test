db08fb334388a044ef88968b2419ecdaf1e2b729
Amaya <jonathan.amaya@softwareone.com>
2023-03-30 13:06:46 -0400

    consolidate steps

--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -6,6 +6,8 @@ import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import navigation.NavigateTo;
+import search_soccer.search.LookForSoccerHeader;
+import search_soccer.search.SoccerArticle;
 import starter.search.LookForInformation;
 import starter.search.WikipediaArticle;
 
@@ -27,6 +29,25 @@ public class SearchStepDefinitions {
     public void should_see_information_about(Actor actor, String term) {
         actor.attemptsTo(
                 Ensure.that(WikipediaArticle.HEADING).hasText(term)
+        );
+    }
+
+    @Given("{actor} is researching soccer on the internet")
+    public void researchingSoccer(Actor actor) {
+        actor.wasAbleTo(NavigateTo.theWikipediaHomePage());
+    }
+
+    @When("{actor} looks up {string}")
+    public void searchesForSoccer(Actor actor, String term) {
+        actor.attemptsTo(
+                LookForSoccerHeader.about(term)
+        );
+    }
+
+    @Then("{actor} should see information about {string}")
+    public void should_see_information_about_soccer(Actor actor, String term) {
+        actor.attemptsTo(
+                Ensure.that(SoccerArticle.HEADING).hasText(term)
         );
     }
 
